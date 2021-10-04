30e1c0b4e8bb5106aa9d8ad5bc55c32d6030623a
lilianafss <liliana.sntss@eduge.ch>
2021-10-04 07:54:44 +0200

    creation dossiers

--- enigme1.sh
+++ enigme1.sh
@@ -3,7 +3,7 @@
 # Script : 
 # Description : 
 #
-# Date : 16/09/2021
+# Date : 27/09/2021
 # Version : 1.0
 # Auteur : Liliana Santos, Ania Marostica
 #****
@@ -14,19 +14,92 @@ dateJour=$(date +%Y-%m-%d)
 
 regex=${dateJour}
 
-declare result;
+declare result
+declare valeurDefaut=6
+declare nomDossier=TOP_SECRET
 
-result=$(grep $regex testCM.csv | cut -d ";" -f "2" )
 
-echo $result
+
+cd ..
+cd ..
+cd /mnt/c/Users/CM2021/Desktop
+
+function dossier(){
+	mkdir $nomDossier 
+	mkdir $nomDossier/A $nomDossier/B $nomDossier/C $nomDossier/D $nomDossier/E
+#*********************** A
+	mkdir $nomDossier/A/B $nomDossier/A/C $nomDossier/A/D $nomDossier/A/E
+	
+	mkdir $nomDossier/A/B/C $nomDossier/A/C/E $nomDossier/A/D/B
+	
+	mkdir $nomDossier/A/C/E/A $nomDossier/A/D/B/D
+	
+	mkdir $nomDossier/A/D/B/D/B
+#*********************** B
+	mkdir $nomDossier/B/A $nomDossier/B/B $nomDossier/B/D
+	
+	mkdir $nomDossier/B/A/D $nomDossier/B/A/C $nomDossier/B/B/A
+	
+	mkdir $nomDossier/B/D/E $nomDossier/B/D/C $nomDossier/B/D/D
+	
+	mkdir $nomDossier/B/D/C/A
+#*********************** C
+	mkdir $nomDossier/C/A $nomDossier/C/F
+	
+	mkdir $nomDossier/C/F/P $nomDossier/C/F/P/T
+#*********************** D
+	mkdir $nomDossier/D/A $nomDossier/D/C $nomDossier/D/E $nomDossier/D/D
+	
+	mkdir $nomDossier/D/C/A
+	
+	mkdir $nomDossier/D/D/B $nomDossier/D/D/D
+#*********************** E
+	mkdir $nomDossier/E/A $nomDossier/E/B $nomDossier/E/C $nomDossier/E/D
+	
+	mkdir $nomDossier/E/A/D
+	
+	mkdir $nomDossier/E/B/A $nomDossier/E/B/C $nomDossier/E/B/E
+	
+	mkdir $nomDossier/E/B/A/C $nomDossier/E/B/A/B $nomDossier/E/B/A/A $nomDossier/E/B/A/D
+	
+	mkdir $nomDossier/E/B/A/C/D
+	
+	mkdir $nomDossier/E/C/A
+	mkdir $nomDossier/E/C/B
+	
+	mkdir $nomDossier/E/C/A/D
+	mkdir $nomDossier/E/C/A/D/C
+	mkdir $nomDossier/E/C/A/D/E
+	
+	mkdir $nomDossier/E/C/A/D/B
+}
+
+if [ -d $nomDossier ]
+then
+	echo " le dossier existe "
+	rm -r $nomDossier
+	dossier
+else
+	dossier
+fi 
+
+cd
+cd enigme1
+
+result=$(grep $regex escapegame_2021.csv | cut -d ";" -f "2" )
+
 
 if [[ -n "$result" ]]
-then
-	cd ..
-	cd ..
-	rm -r /mnt/c/Users/liliana.sntss/Desktop/test/*
-	cp /mnt/c/Users/liliana.sntss/Pictures/$result.png /mnt/c/Users/liliana.sntss/Desktop/test/$result.png
+then 
+	rm -r /mnt/c/Users/CM2021/Desktop/$nomDossier/E/B/A/C/D/*
+	cp /mnt/c/Users/CM2021/Desktop/imagesEnigme/$result.PNG /mnt/c/Users/CM2021/Desktop/$nomDossier/E/B/A/C/D/$result.PNG
+	cp /mnt/c/Users/CM2021/Desktop/imagesEnigme/ListeDeMateriel /mnt/c/Users/CM2021/Desktop/$nomDossier/E/C/A/D/B/ListeDeMateriel
 else
-	echo "la variable est vide"
+	echo $valeurDefaut
+	rm -r /mnt/c/Users/CM2021/Desktop/$nomDossier/E/B/A/C/D/*
+	cp /mnt/c/Users/CM2021/Desktop/imagesEnigme/$valeurDefaut.PNG /mnt/c/Users/CM2021/Desktop/$nomDossier/E/B/A/C/D/$valeurDefaut.PNG
+	cp /mnt/c/Users/CM2021/Desktop/imagesEnigme/ListeDeMateriel.txt /mnt/c/Users/CM2021/Desktop/$nomDossier/E/C/A/D/B/ListeDeMateriel.txt
 fi
+
+
 
